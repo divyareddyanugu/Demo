9f9da2b8828c5c768086c12dc14685fdf7e72864
a.divyareddy@gmail.com <a.divyareddy@gmail.com@c04856b0-da88-c79f-09e7-89a47f2d9e1f>
2012-05-15 18:56:45 +0000

    Adding complete code. All 3 projects. Android application, Server application and Aug Reality engine

---
+++ trunk/sfmtserver/src/XMLHandler.java
@@ -0,0 +1,13 @@
+import java.io.Serializable;
+
+/**
+ * @author Graham O'Regan @ <a href="http://www.ellisonbrookes.com">Ellison Brookes</a>
+ */
+public class XMLHandler implements Serializable {
+
+    public String echo(String input) {
+
+        return input;
+    }
+
+}
